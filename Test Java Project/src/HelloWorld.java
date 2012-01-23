e87f254c5eb4ef20d126b5172986c9e83e2f8bcd
Matan Peled <mip@cs.technion.ac.il>
2012-01-23 11:26:25 +0200

    Small test

--- Test Java Project/src/HelloWorld.java
+++ Test Java Project/src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("hello, world!\n");
+		System.out.println("Hello, World!\n");
 	}
 
 }
