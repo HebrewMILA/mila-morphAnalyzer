f87e08254ae346ee3545047be36198617cb9e8c7
Matan Peled <mip@cs.technion.ac.il>
2012-04-23 17:07:36 +0300

    add mila-generator playground

---
+++ mila-generator/src/main/java/org/mila/lexicon/helpers/ItemHelper.java
@@ -0,0 +1,123 @@
+package org.mila.lexicon.helpers;
+
+import org.mila.lexicon.entities.ItemType;
+
+public class ItemHelper {
+    public static SubItemType getSubItem(ItemType item) {
+	switch (ItemHelper.getSubItemEnum(item)) {
+	case ACRONYM:
+	    return item.getAcronym();
+	case ADJECTIVE:
+	    return item.getAdjective();
+	case ADVERB:
+	    return item.getAdverb();
+	case CONJUCTION:
+	    return item.getConjunction();
+	case COPULA:
+	    return item.getCopula();
+	case EXISTENTIAL:
+	    return item.getExistential();
+	case IMPERSONAL:
+	    return item.getImpersonal();
+	case INTERJECTION:
+	    return item.getInterjection();
+	case INTERROGATIVE:
+	    return item.getInterrogative();
+	case MODAL:
+	    return item.getModal();
+	case MULTI_WORD_FROZEN:
+	    return item.getMultiWordFrozen();
+	case MULTI_WORD_NOUN:
+	    return item.getNoun();
+	case MULTI_WORD_NOUN_ADJECTIVE:
+	    return item.getMultiWordNounAdjective();
+	case MULTI_WORD_PREPOSITION:
+	    return item.getMultiWordPreposition();
+	case MULTI_WORD_VERB_PHRASE:
+	    return item.getMultiWordVerbPhrase();
+	case NEGATION:
+	    return item.getNegation();
+	case NOUN:
+	    return item.getNoun();
+	case NUMERAL:
+	    return item.getNumeral();
+	case PREPOSITION:
+	    return item.getPreposition();
+	case PRONOUN:
+	    return item.getPronoun();
+	case PROPER_NAME:
+	    return item.getProperName();
+	case QUANTIFIER:
+	    return item.getQuantifier();
+	case TITLE:
+	    return item.getTitle();
+	case VERB:
+	    return item.getVerb();
+	case W_PREFIX:
+	    return item.getWPrefix();
+	default:
+	    throw new SwitchException();
+	}
+
+    }
+
+    public static SubItemEnum getSubItemEnum(ItemType item) {
+	/* Assumption - Only one of these can be true at any given time. */
+	if (item.isSetAcronym())
+	    return SubItemEnum.ACRONYM;
+	if (item.isSetAdjective())
+	    return SubItemEnum.ADJECTIVE;
+	if (item.isSetAdverb())
+	    return SubItemEnum.ADVERB;
+	if (item.isSetConjunction())
+	    return SubItemEnum.CONJUCTION;
+	if (item.isSetCopula())
+	    return SubItemEnum.COPULA;
+	if (item.isSetExistential())
+	    return SubItemEnum.EXISTENTIAL;
+	if (item.isSetImpersonal())
+	    return SubItemEnum.IMPERSONAL;
+	if (item.isSetInterjection())
+	    return SubItemEnum.INTERJECTION;
+	if (item.isSetInterrogative())
+	    return SubItemEnum.INTERROGATIVE;
+	if (item.isSetModal())
+	    return SubItemEnum.MODAL;
+	if (item.isSetMultiWordFrozen())
+	    return SubItemEnum.MULTI_WORD_FROZEN;
+	if (item.isSetMultiWordNoun())
+	    return SubItemEnum.MULTI_WORD_NOUN;
+	if (item.isSetMultiWordNounAdjective())
+	    return SubItemEnum.MULTI_WORD_NOUN_ADJECTIVE;
+	if (item.isSetMultiWordPreposition())
+	    return SubItemEnum.MULTI_WORD_PREPOSITION;
+	if (item.isSetMultiWordVerbPhrase())
+	    return SubItemEnum.MULTI_WORD_VERB_PHRASE;
+	if (item.isSetNegation())
+	    return SubItemEnum.NEGATION;
+	if (item.isSetNoun())
+	    return SubItemEnum.NOUN;
+	if (item.isSetNumeral())
+	    return SubItemEnum.NUMERAL;
+	if (item.isSetPreposition())
+	    return SubItemEnum.PREPOSITION;
+	if (item.isSetPronoun())
+	    return SubItemEnum.PRONOUN;
+	if (item.isSetProperName())
+	    return SubItemEnum.PROPER_NAME;
+	if (item.isSetQuantifier())
+	    return SubItemEnum.QUANTIFIER;
+	if (item.isSetTitle())
+	    return SubItemEnum.TITLE;
+	if (item.isSetVerb())
+	    return SubItemEnum.VERB;
+	if (item.isSetWPrefix())
+	    return SubItemEnum.W_PREFIX;
+
+	throw new SwitchException();
+    }
+}
+
+class SwitchException extends RuntimeException {
+    private static final long serialVersionUID = -8704399381177684939L;
+}
---
+++ mila-generator/src/main/java/org/mila/lexicon/helpers/SubItemType.java
@@ -0,0 +1,12 @@
+package org.mila.lexicon.helpers;
+
+import java.io.Serializable;
+
+/**
+ * @author Matan Peled
+ * 
+ * Dummy interface, so that we can refer to all the sub-item entity types by
+ * some common means.
+ */
+public interface SubItemType extends Serializable {
+}
