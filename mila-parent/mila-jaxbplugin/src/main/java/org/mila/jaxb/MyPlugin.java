29393d9cfdb62ab791003370dbc38f5f5d56825d
Matan Peled <mip@cs.technion.ac.il>
2013-01-29 11:57:39 +0200

    jaxb plugin fixes

--- mila-parent/mila-jaxbplugin/src/main/java/org/mila/jaxb/MyPlugin.java
+++ mila-parent/mila-jaxbplugin/src/main/java/org/mila/jaxb/MyPlugin.java
@@ -16,58 +16,67 @@ import com.sun.tools.xjc.outline.Outline;
 
 public class MyPlugin extends Plugin {
 
-    @Override
-    public String getOptionName() {
-	return "Xmyplugin";
-    }
-
-    @Override
-    public String getUsage() {
-	return "  -Xmyplugin      :  do some mila specific fixes";
-    }
-
-    @Override
-    public boolean run(Outline model, Options arg1, ErrorHandler arg2)
-	    throws SAXException {
-	try {
-	    for (ClassOutline co : model.getClasses()) {
-		for (JMethod method : co.ref.methods()) {
-		    int transientCount = 0;
-		    ArrayList<JAnnotationUse> toRemove = new ArrayList<JAnnotationUse>();
-		    for (JAnnotationUse anno : method.annotations()) {
-			if (anno.getAnnotationClass().toString()
-				.contains("Transient")) {
-			    transientCount++;
-			    toRemove.add(anno);
-			}
-		    }
-		    if (transientCount <= 1)
-			continue;
-
-		    Field f = method.getClass().getDeclaredField("annotations");
-		    f.setAccessible(true);
-
-		    @SuppressWarnings("unchecked")
-		    Collection<JAnnotationUse> annos = (Collection<JAnnotationUse>) f
-			    .get(method);
-		    annos.removeAll(toRemove);
-
-		}
-
-	    }
-	} catch (SecurityException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} catch (NoSuchFieldException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} catch (IllegalArgumentException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} catch (IllegalAccessException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	@Override
+	public String getOptionName() {
+		return "Xmyplugin";
 	}
-	return true;
-    }
+
+	@Override
+	public String getUsage() {
+		return "  -Xmyplugin      :  do some mila specific fixes";
+	}
+
+	@Override
+	public boolean run(Outline model, Options arg1, ErrorHandler arg2)
+			throws SAXException {
+		try {
+			for (ClassOutline co : model.getClasses()) {
+				for (JMethod method : co.ref.methods()) {
+					boolean killTrans = false;
+					ArrayList<JAnnotationUse> toRemove = new ArrayList<JAnnotationUse>();
+					for (JAnnotationUse anno : method.annotations()) {
+						anno.getAnnotationClass();
+						if (anno.getAnnotationClass().toString()
+								.contains("Deprecated")) {
+							killTrans = true;
+							toRemove.add(anno);
+						}
+					}
+					if (!killTrans)
+						continue;
+					for (JAnnotationUse anno : method.annotations()) {
+						anno.getAnnotationClass();
+						if (anno.getAnnotationClass().toString()
+								.contains("Transient")) {
+							killTrans = true;
+							toRemove.add(anno);
+						}
+					}
+
+
+					Field f = method.getClass().getDeclaredField("annotations");
+					f.setAccessible(true);
+
+					@SuppressWarnings("unchecked")
+					Collection<JAnnotationUse> annos = (Collection<JAnnotationUse>) f
+							.get(method);
+					annos.removeAll(toRemove);
+				}
+
+			}
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
 }
