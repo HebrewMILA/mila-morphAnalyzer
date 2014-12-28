5ef5cff85b3d4e415bc89b53f69c933f7f4ab987
Slava Demender <slav4ik@cs.technion.ac.il>
2014-12-28 19:56:31 +0200

    Changed maintainer to Slava

--- MorphologicalAnalyzer/src/mila/mw/MWCreateCorpusXML.java
+++ MorphologicalAnalyzer/src/mila/mw/MWCreateCorpusXML.java
@@ -19,7 +19,7 @@ import mila.generated.ObjectFactory;
 
 /**
  * @author daliabo
- * 
+ *
  *         TODO To change the template for this generated type comment go to
  *         Window - Preferences - Java - Code Style - Code Templates
  */
@@ -38,44 +38,48 @@ public class MWCreateCorpusXML extends CreateCorpusXML {
 		analysisCounter++;
 		try {
 			analysis = objFactory.createAnalysisType();
-		} catch (JAXBException e) {
+		} catch (final JAXBException e) {
 			System.out
-					.println("CreateCorpusXML:createImpersonalAnalysis Exception while creating analysis for hebWord="
-							+ hebWord);
+			.println("CreateCorpusXML:createImpersonalAnalysis Exception while creating analysis for hebWord="
+					+ hebWord);
 			e.printStackTrace();
 		}
 		analysis.setId(String.valueOf(analysisCounter));
 		BaseType base = null;
 		try {
 			base = objFactory.createBaseType();
-		} catch (JAXBException e1) {
+		} catch (final JAXBException e1) {
 			System.out
-					.println("CreateCorpusXML:createImpersonalAnalysis Exception while creating base for hebWord="
-							+ hebWord);
+			.println("CreateCorpusXML:createImpersonalAnalysis Exception while creating base for hebWord="
+					+ hebWord);
 			e1.printStackTrace();
 		}
-		if (!description.equals(""))
+		if (!description.equals("")) {
 			setPrefix(description);
+		}
 
 		setBase(base, transliteratedLexiocnItem, hebWord, "", hebWord);
 		MWEType mwe1 = null;
 		try {
 			mwe1 = objFactory.createMWEType();
-		} catch (JAXBException e2) {
+		} catch (final JAXBException e2) {
 			System.out
-					.println("CreateCorpusXML:createImpersonalAnalysis Exception while creating ImpersonalType for hebWord="
-							+ hebWord);
+			.println("CreateCorpusXML:createImpersonalAnalysis Exception while creating ImpersonalType for hebWord="
+					+ hebWord);
 			e2.printStackTrace();
 		}
 		mwe1.setConsecutive("true");
-		if (mweid.length() == 1 && mweid.charAt(0) == '1')
+		if (mweid.length() == 1 && mweid.charAt(0) == '1') {
 			mwe1.setPos(pos);
+		}
 		mwe1.setId(mweid);
-		if (!definiteness.equals("unspecified"))
+		if (!definiteness.equals("unspecified")) {
 			mwe1.setDefiniteness(definiteness);
+		}
 		mwe1.setMultiWordPrefixExist(prefix);
-		if (pos.equalsIgnoreCase("propername") && type.charAt(0) != 'u')
+		if (pos.equalsIgnoreCase("propername") && type.charAt(0) != 'u') {
 			mwe1.setType(type);
+		}
 		base.setMWE(mwe1);
 		analysis.setBase(base);
 		token.getAnalysis().add(analysis);
@@ -86,37 +90,37 @@ public class MWCreateCorpusXML extends CreateCorpusXML {
 		// System.out.println("(F) createXMLdOC() ");
 		try {
 			jc = JAXBContext.newInstance("mila.generated");
-		} catch (JAXBException e) {
+		} catch (final JAXBException e) {
 			System.out
-					.println("CreateCorpusXML:createXMLdOC Exception while creating JAXBContext");
+			.println("CreateCorpusXML:createXMLdOC Exception while creating JAXBContext");
 			e.printStackTrace();
 		}
 		objFactory = new ObjectFactory();
 		try {
 			corpus = objFactory.createCorpus();
-		} catch (JAXBException e1) {
+		} catch (final JAXBException e1) {
 			System.out
-					.println("CreateCorpusXML:createXMLdOC Exception while creating corpus");
+			.println("CreateCorpusXML:createXMLdOC Exception while creating corpus");
 			e1.printStackTrace();
 		}
 		String hostname = "unknown.unknown";
 		try {
 			hostname = InetAddress.getLocalHost().getHostName();
-		} catch (Throwable t) {
+		} catch (final Throwable t) {
 
 		}
 		corpus.setName("Analysis Results (" + hostname + ") @ "
 				+ Calendar.getInstance().getTime());
-		corpus.setMaintainer("Matan Peled");
+		corpus.setMaintainer("Slava Demender");
 		corpus.setEmail("mila@cs.technion.ac.il");
 
 		corpus.setComment("versions info: lexicon: 13/03/2013;  morphologicalAnalyzer: 1.8 (13/03/2013); "
 				+ "corpus schema 16/06/2009; lexicon schema 16/06/2009");
 		try {
 			validator = jc.createValidator();
-		} catch (JAXBException e2) {
+		} catch (final JAXBException e2) {
 			System.out
-					.println("CreateCorpusXML:createXMLdOC Exception while creating Validator");
+			.println("CreateCorpusXML:createXMLdOC Exception while creating Validator");
 			e2.printStackTrace();
 		}
 	}
@@ -125,8 +129,9 @@ public class MWCreateCorpusXML extends CreateCorpusXML {
 			String lexiconItem, String lexiconPointer, String dottedLexiconItem) {
 		base.setTransliteratedLexiconItem(transliteratedLexiocnItem);
 		base.setLexiconItem(lexiconItem);
-		if (lexiconPointer.length() > 0)
+		if (lexiconPointer.length() > 0) {
 			base.setLexiconPointer(lexiconPointer);
+		}
 		base.setDottedLexiconItem(null);
 	}
 
