0c7650d8f083aa24fcac4787e43f49e7f59b0e7c
Slava Demender <slav4ik@cs.technion.ac.il>
2014-12-28 20:10:44 +0200

    Upated maintainer to Slava Changed ouput of errors to stderr instead of stdout

--- MorphologicalAnalyzer/src/mila/tokenizers/XMLProcessor.java
+++ MorphologicalAnalyzer/src/mila/tokenizers/XMLProcessor.java
@@ -27,233 +27,137 @@ import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 
 /**
- * 
- * 
- * 
+ *
+ *
+ *
  * XMLProcessor.java Purpose: build the XML of the output of the tokenizer using
  * j4dom
- * 
- * 
- * 
+ *
+ *
+ *
  * @author Dalia Bojan
- * 
+ *
  * @version %G%
- * 
+ *
  */
-
 public class XMLProcessor {
 
 	/**
-	 * 
+	 *
 	 * @param heb
-	 * 
+	 *
 	 * @return
-	 * 
+	 *
 	 */
-
 	public static String getHebToEng(String heb) {
-
 		if (hebToEng == null) {
-
 			initHebToEng();
-
 		}
-
-		return (String) hebToEng.get(heb);
-
+		return hebToEng.get(heb);
 	}
 
 	/**
-	 * 
+	 *
 	 * @param hebStr
-	 * 
+	 *
 	 * @return
-	 * 
+	 *
 	 */
-
 	private static String translateHebEng(String hebStr) {
-
-		StringBuffer transliterated = new StringBuffer();
-
+		final StringBuffer transliterated = new StringBuffer();
 		String transliteratedStr = "";
-
 		char curChar;
-
 		char transChar;
-
 		for (int i = 0; i < hebStr.length(); i++) {
-
 			curChar = hebStr.charAt(i);
-
-			if ((curChar >= 'א') && (curChar <= 'ת')) {
-
-				String charStr = (new Character(curChar)).toString();
-
+			if (curChar >= 'א' && curChar <= 'ת') {
+				final String charStr = new Character(curChar).toString();
 				transChar = getHebToEng(charStr).charAt(0);
-
 				transliterated.append(transChar);
-
-			}
-
-			else
-
+			} else {
 				transliterated.append(curChar);
-
+			}
 		}
-
 		transliteratedStr = transliterated.toString();
-
 		return transliteratedStr;
-
 	}
 
 	Document document;
-
 	int countSentence = 0;
-
 	int countParagraph = 0;
-
 	int countToken = 0;
-
 	Element root;
-
 	Element article;
-
 	Element paragraph;
-
 	Element sentence;
-
 	Element token;
-
 	int globalTokenCounter = 0;
-
-	private static HashMap hebToEng;
+	private static HashMap<String, String> hebToEng;
 
 	private static void initHebToEng() {
-
-		hebToEng = new HashMap();
-
+		hebToEng = new HashMap<>();
 		hebToEng.put("א", "a");
-
 		hebToEng.put("ב", "b");
-
 		hebToEng.put("ג", "g");
-
 		hebToEng.put("ד", "d");
-
 		hebToEng.put("ה", "h");
-
 		hebToEng.put("ו", "w");
-
 		hebToEng.put("ז", "z");
-
 		hebToEng.put("ח", "x");
-
 		hebToEng.put("ט", "v");
-
 		hebToEng.put("י", "i");
-
 		hebToEng.put("כ", "k");
-
 		hebToEng.put("ך", "k");
-
 		hebToEng.put("ל", "l");
-
 		hebToEng.put("מ", "m");
-
 		hebToEng.put("ם", "m");
-
 		hebToEng.put("נ", "n");
-
 		hebToEng.put("ן", "n");
-
 		hebToEng.put("ס", "s");
-
 		hebToEng.put("ע", "y");
-
 		hebToEng.put("פ", "p");
-
 		hebToEng.put("ף", "p");
-
 		hebToEng.put("צ", "c");
-
 		hebToEng.put("ץ", "c");
-
 		hebToEng.put("ק", "q");
-
 		hebToEng.put("ר", "r");
-
 		hebToEng.put("ש", "e");
-
 		hebToEng.put("ת", "t");
-
 	}
 
 	public void createArticle() {
-
 		article = root.addElement("article");
-
 		article.addAttribute("id", "1");
-
 	}
 
 	/**
-	 * 
-	 * 
-	 * 
+	 *
+	 *
+	 *
 	 */
-
 	public void createDocument() {
-
 		document = DocumentHelper.createDocument();
-
 		root = document.addElement("corpus");
-
 		root.addAttribute("name", "Demo Hebrew tokenized text in XML");
-
-		root.addAttribute("version", "1.7.0").addAttribute("maintainer",
-
-		"Matan Peled").addAttribute("email", "mila@cs.technion.ac.il");
-
-		Element metadata = root.addElement("metadata");
-
+		root.addAttribute("version", "1.7.0")
+				.addAttribute("maintainer", "Slava Demender")
+				.addAttribute("email", "mila@cs.technion.ac.il");
+		final Element metadata = root.addElement("metadata");
 		metadata.addElement("name")
-
-		.addText("Demo Hebrew tokenized text in XML");
-
+				.addText("Demo Hebrew tokenized text in XML");
 		metadata.addElement("version").addText("1.0");
-
-		Date now = new Date();
-
+		final Date now = new Date();
 		metadata.addElement("date").addText(
-
-		DateFormat.getDateTimeInstance(DateFormat.SHORT,
-
-		DateFormat.SHORT).format(now));
-
-		metadata.addElement("maintainer").addText("Matan Peled");
-
+				DateFormat.getDateTimeInstance(DateFormat.SHORT,
+						DateFormat.SHORT).format(now));
+		metadata.addElement("maintainer").addText("Slava Demender");
 		metadata.addElement("email").addText("mila@cs.technion.ac.il");
-
-		metadata
-
-		.addElement("license")
-
+		metadata.addElement("license")
 				.addText(
-
 						"Tokenization and XML representation: Copyright (C) 2009 Mila.\n"
-
-								+
-
-								"This resource is released to the public licensed under the GNU Free Documentation License\n."
-
-								+
-
-								"Note that all software, data files and documentation are licensed under the FDL.\n"
-
-								+
-
-								"There is no warranty of any kind for the contents of this distribution.");
+								+ "This resource is released to the public licensed under the GNU Free Documentation License\n."
+								+ "Note that all software, data files and documentation are licensed under the FDL.\n"
+								+ "There is no warranty of any kind for the contents of this distribution.");
 		/*
 		 * Element transliteration =
 		 * metadata.addElement("transliteration").addAttribute("from", "he");
@@ -346,192 +250,121 @@ public class XMLProcessor {
 
 	public void createParapraphes() {
 
-		if ((countToken == 0) && (countParagraph > 0))
-
+		if (countToken == 0 && countParagraph > 0) {
 			paragraph.remove(sentence);
-
+		}
 		countToken = 0;
-
 		countSentence = 0;
-
 		countParagraph++;
-
-		String countst = String.valueOf(countParagraph);
-
+		final String countst = String.valueOf(countParagraph);
 		paragraph = article.addElement("paragraph").addAttribute("id", countst);
-
 	}
 
 	public void createSentences() {
-
 		countToken = 0;
-
 		countSentence++;
-
-		String countst = String.valueOf(countSentence);
-
+		final String countst = String.valueOf(countSentence);
 		sentence = paragraph.addElement("sentence").addAttribute("id", countst);
-
 	}
 
 	/**
-	 * 
+	 *
 	 * @param word
-	 * 
+	 *
 	 */
-
 	public void createTokens(String word) {
-
 		countToken++;
-
 		globalTokenCounter++;
-
-		String counttk = String.valueOf(countToken);
-
-		String transliterated = translateHebEng(word);
-
+		final String counttk = String.valueOf(countToken);
+		final String transliterated = translateHebEng(word);
 		try {
-
 			token = sentence.addElement("token").addAttribute("id", counttk)
-
-			.addAttribute("surface", word).addAttribute(
-
-			"transliterated", transliterated);
-
+					.addAttribute("surface", word)
+					.addAttribute("transliterated", transliterated);
 			// System.out.println(word);
-
-		} catch (Exception e) {
-
-			// TODO Auto-generated catch block
-
+		} catch (final Exception e) {
 			e.printStackTrace();
-
 		}
-
 	}
 
 	public void finalizeDoc() {
-
-		if ((countToken == 0) && (countParagraph > 0))
-
+		if (countToken == 0 && countParagraph > 0) {
 			paragraph.remove(sentence);
-
+		}
 	}
 
 	/**
-	 * 
+	 *
 	 * @return Returns the document.
-	 * 
+	 *
 	 */
-
 	public Document getDocument() {
-
 		return document;
-
 	}
 
 	/**
-	 * 
+	 *
 	 * @return Returns the globalTokenCounter.
-	 * 
+	 *
 	 */
-
 	public int getGlobalTokenCounter() {
-
 		return globalTokenCounter;
-
 	}
 
 	public String printDoc() {
-
-		if ((countToken == 0) && (countParagraph > 0))
-
+		if (countToken == 0 && countParagraph > 0) {
 			paragraph.remove(sentence);
-
+		}
 		return document.asXML();
-
 	}
 
 	/**
-	 * 
+	 *
 	 * @param outputFileName
-	 * 
+	 *
 	 */
-
 	public void printDoc(String outputFileName) {
-
-		if ((countToken == 0) && (countParagraph > 0))
-
+		if (countToken == 0 && countParagraph > 0) {
 			paragraph.remove(sentence);
-
-		String text = document.asXML();
-
+		}
+		final String text = document.asXML();
 		FileOutputStream out = null;
-
 		try {
-
 			out = new FileOutputStream(outputFileName);
-
-		} catch (FileNotFoundException e) {
-
-			System.out
-
-			.println("XMLProcessor: printDoc - FileNotFoundException");
-
+		} catch (final FileNotFoundException e) {
+			System.err
+					.println("XMLProcessor: printDoc - FileNotFoundException");
 			e.printStackTrace();
-
 		}
-
 		OutputStreamWriter pOut = null;
-
 		try {
-
 			pOut = new OutputStreamWriter(out, "UTF8");
-
-		} catch (UnsupportedEncodingException e4) {
-
-			System.out
-
-			.println("XMLProcessor: printDoc - UnsupportedEncodingException");
-
+		} catch (final UnsupportedEncodingException e4) {
+			System.err
+					.println("XMLProcessor: printDoc - UnsupportedEncodingException");
 			e4.printStackTrace();
-
 		}
 
-		BufferedWriter bw = new BufferedWriter(pOut);
-
+		final BufferedWriter bw = new BufferedWriter(pOut);
 		try {
-
 			bw.write(text);
-
 			bw.close();
-
 			pOut.close();
-
-		} catch (IOException e3) {
-
-			System.out
-
+		} catch (final IOException e3) {
+			System.err
 					.println("XMLProcessor: printDoc - Exception while writing/closing buffered writer");
-
 			e3.printStackTrace();
-
 		}
-
 	}
 
 	/**
-	 * 
+	 *
 	 * @param document
-	 * 
+	 *
 	 *            The document to set.
-	 * 
+	 *
 	 */
-
 	public void setDocument(Document document) {
-
 		this.document = document;
-
 	}
-
 }
