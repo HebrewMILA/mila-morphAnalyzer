1283fe40f3eab111f0dfc7a95af6c5afd62dcbf5
Slava Demender <slav4ik@cs.technion.ac.il>
2015-03-20 10:01:39 +0200

    Removed redundant comments and prints

--- MorphologicalAnalyzer/src/mila/mw/MWData.java
+++ MorphologicalAnalyzer/src/mila/mw/MWData.java
@@ -15,7 +15,7 @@ import mila.lexicon.dbUtils.MWEinflectionsRecord;
 
 /**
  * @author daliabo
- * 
+ *
  *         TODO To change the template for this generated type comment go to
  *         Window - Preferences - Java - Code Style - Code Templates
  */
@@ -23,20 +23,16 @@ public class MWData extends Data {
 
 	public static ArrayList<MWEinflectionsRecord> getMWEinflections(String key)
 			throws Exception {
-		// System.out.println("(F)MWData:getMWEinflections("+key+")");
 		ArrayList<MWEinflectionsRecord> mweList = new ArrayList<MWEinflectionsRecord>();
 		ArrayList<MWEinflectionsRecord> returnedMweList = new ArrayList<MWEinflectionsRecord>();
 		int mweListSize = 0;
 		MWEinflectionsRecord mwinfRec = null;
 		// get from databse
 		if (webFlag) {
-			System.out
-					.println("(F) MWData:getMWEinflections() webFlag = TRUE ");
 			mila.lexicon.dbUtils.MWEinflections mweInf = new mila.lexicon.dbUtils.MWEinflections();
 			mweList = MWEinflections.get(key);
 			// get from lists
 		} else {
-			// System.out.println("(F) getMWEinflections()  get from inflection files !!!");
 			// get from inflection files !!!
 			mweList = mwinflections.get(key);
 		}
@@ -49,8 +45,6 @@ public class MWData extends Data {
 			mwinfRec = new MWEinflectionsRecord();
 			mwinfRec = mweList.get(MWEListIndex);
 			String surface = URLDecoder.decode(mwinfRec.getSurface(), "UTF-8");
-			// System.out.println("(F) getMWEinflections: surface = " +
-			// surface);
 			mwinfRec.setSurface(surface);
 			returnedMweList.add(mwinfRec);
 		}
