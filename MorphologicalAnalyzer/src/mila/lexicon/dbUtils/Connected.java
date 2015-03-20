37fd084f2911647eb7cfaf5762568621255dc247
Slava Demender <slav4ik@cs.technion.ac.il>
2015-03-20 10:04:27 +0200

    Changed to work more statically, releasing connection after each query

--- MorphologicalAnalyzer/src/mila/lexicon/dbUtils/Connected.java
+++ MorphologicalAnalyzer/src/mila/lexicon/dbUtils/Connected.java
@@ -3,6 +3,7 @@ package mila.lexicon.dbUtils;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -26,7 +27,7 @@ import snaq.db.ConnectionPool;
  * In order to learn more about the connection pool configuration, please seek
  * <b>bitmechanic</b> and find out.
  * <p>
- * 
+ *
  * @author Danny Shacham
  * @version 1.0
  */
@@ -40,15 +41,16 @@ public class Connected {
 	private static ConnectionPool pool = null;
 	static {
 		try {
-			Driver driver = (Driver) Class.forName("org.mariadb.jdbc.Driver").newInstance();
+			Driver driver = (Driver) Class.forName("org.mariadb.jdbc.Driver")
+					.newInstance();
 			DriverManager.registerDriver(driver);
 			// pool = new ConnectionPool("mysqlLexiocn", 10, 20, 18000, myurl,
 			// mysqlUser, mysqlPassword);
-			
+
 			// pool = new ConnectionPool("mysqlLexiocn", 10, 20, 18000,
 			// "jdbc:mysql://yeda.cs.technion.ac.il:3306/generator",
 			// "dummy1", "health&happiness");
-			
+
 			// pool = new ConnectionPool("mysqlLexiocn", 10, 20, 18000,
 			// "jdbc:mysql://localhost/lexicon_06_08_09",
 			// "root", "dalia");
@@ -63,7 +65,7 @@ public class Connected {
 			// pool = new ConnectionPool("mysqlLexiocn", 10, 20, 18000,
 			// "jdbc:mysql://yeda.cs.technion.ac.il:3306/mwGenerator",
 			// "gili", "dr7R2c8edru");
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(0);
@@ -75,7 +77,7 @@ public class Connected {
 	 * A Connection object used by the object and it's extentions in order to
 	 * connect to the DB. The connection is created and released by the
 	 * <code>ConnectionManager</code> object.
-	 * 
+	 *
 	 * @see ConnectionManager
 	 */
 	protected static Connection conn = null;
@@ -85,7 +87,7 @@ public class Connected {
 	 * the DB. The object must be closed after the execution of the DB
 	 * transaction, using <code>stmt.close()</code>. The object is created by
 	 * the <code>Connection</code> object.
-	 * 
+	 *
 	 * @see java.sql.Connection
 	 * @see #conn
 	 */
@@ -94,26 +96,27 @@ public class Connected {
 	/**
 	 * Serves as a super method for DB actions that require an "execute" mode,
 	 * such as INSERT ,UPDATE or DELETE.
-	 * 
+	 *
 	 * @param sql
 	 *            The SQL statement to be executed.
 	 * @return Number of rows affected (0, if nothing happened, 1 if one row
 	 *         added, ..., -1 if the statement is a SELECT statement).
 	 */
 	protected static int execute(String sql) {
-		if (sql == null)
+		if (sql == null) {
 			return 0;
+		}
 		int feedback = 0;
 		try {
-			System.out.println("Preparing connection for query: "+sql);
-			prepareConnection();
+			System.out.println("Preparing connection for query: " + sql);
+			conn = prepareConnection();
 			stmt = conn.createStatement();
 			feedback = stmt.executeUpdate(sql);
 		} catch (SQLException E) {
 			System.out.println(sql);
 			E.printStackTrace();
 		} finally {
-			System.out.println("Releasing connection for query: "+sql);
+			System.out.println("Releasing connection for query: " + sql);
 			releaseConnection();
 		}
 		return feedback;
@@ -130,12 +133,12 @@ public class Connected {
 	 * variable <code>directConnection</code> is used, then the method would
 	 * open a conenction directly, using the
 	 * <code>prepareDirectConnection</code> method.
-	 * 
+	 *
 	 * @see DriverManager#getConnection
 	 * @see #conn
 	 * @see #prepareDirectConnection
 	 */
-	protected static void prepareConnection() throws SQLException {
+	protected static Connection prepareConnection() throws SQLException {
 		/*
 		 * if (cpds == null) { Context ctx = null; try {
 		 * System.out.println("*****   Starting the DB connection!   ****"); ctx
@@ -143,13 +146,17 @@ public class Connected {
 		 * System.out.println("Boom - no cotext"); Context envCtx = (Context)
 		 * ctx.lookup("java:comp/env"); DataSource ds = (DataSource)
 		 * envCtx.lookup("jdbc/mysqlDBlexicon"); Connected.setCPDS(ds);
-		 * 
+		 *
 		 * } catch (NamingException ne) {
 		 * System.out.println("Exception in creating the DataSource!");
 		 * ne.printStackTrace(); } } conn = getCPDS().getConnection();
 		 */
 		long timeout = 2000; // 2 second timeout
-		conn = pool.getConnection(timeout);
+		Connection conn = pool.getConnection(timeout);
+		if (conn == null){
+			throw new SQLException();
+		}
+		return conn;
 	}
 
 	/**
@@ -157,7 +164,7 @@ public class Connected {
 	 * releasing is done by calling <code>conn.close()</code>. Please note that
 	 * the use of some conenction pooling devices may cause the connection not
 	 * to be actually closed, even if this action is performed.
-	 * 
+	 *
 	 * @see Connection#close
 	 * @see #prepareConenction
 	 */
@@ -187,15 +194,43 @@ public class Connected {
 	protected static ResultSet staticGetData(String sql) {
 		ResultSet rs = null;
 		try {
-			prepareConnection();
+			conn = prepareConnection();
 			stmt = conn.createStatement();
 			stmt.execute(sql);
 			rs = stmt.getResultSet();
 		} catch (SQLException E) {
 			System.out.println("Lexicon Message: Content.getData Error" + sql);
 			E.printStackTrace();
+		}finally{
+			releaseConnection();
 		}
 		return rs;
+	}
+	protected static ResultSet staticGetData(String query, String... params) {
+		ResultSet rs = null;
+		try {
+			conn = prepareConnection();
+			PreparedStatement stmt = conn.prepareStatement(query);
+			for (int i = 1; i <= params.length; i++) {
+				stmt.setString(i, params[i - 1]);
+			}
+			stmt.execute();
+			rs = stmt.getResultSet();
+		} catch (SQLException E) {
+			String paramsOut = "";
+			for (String param : params){
+				paramsOut += " "+param;
+
+			}
+			System.err
+					.println("Lexicon Message: Content.getData Error trying to excute:\n"
+							+ query + paramsOut);
+			E.printStackTrace();
+		} finally{
+			releaseConnection();
+		}
+		return rs;
+
 	}
 
 	/**
@@ -210,25 +245,17 @@ public class Connected {
 	 * . There might be a performence problem becuase the method does not close
 	 * the <code>ResultSet</code>, <code>Statement</code> and
 	 * <code>Connection</code> objects.
-	 * 
+	 *
 	 * @param sql
 	 *            The SQL statement to be executed
 	 * @return The ResultSet recieved from the DB
 	 */
 	protected ResultSet getData(String sql) {
-		ResultSet rs = null;
-		try {
-			prepareConnection();
-			stmt = conn.createStatement();
-			stmt.execute(sql);
-			rs = stmt.getResultSet();
-		} catch (SQLException E) {
-			System.out
-					.println("Lexicon Message: Content.getData Error trying to excute: "
-							+ sql);
-			E.printStackTrace();
-		}
-		return rs;
+		return Connected.staticGetData(sql);
+	}
+
+	protected ResultSet getData(String query, String... params) {
+		return Connected.staticGetData(query, params);
 	}
 
 }
